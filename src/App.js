b6c653fbd199bad629a0117822c79296cc6e440f
daniel makarzec <danielmakarzec@gmail.com>
2022-03-31 15:01:02 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { useState } from 'react';
-import { ThemeProvider } from 'styled-components'
-import { LightTheme, DarkTheme } from './style/themes'
+import React, {
+  useState
+} from 'react';
+import {
+  ThemeProvider
+} from 'styled-components'
+import {
+  LightTheme,
+  DarkTheme
+} from './style/themes'
 import Page from './Page';
 
 function App() {
   const [darkModeOn, setDarkModeOn] = useState(false)
 
-  return (
-    <ThemeProvider theme={darkModeOn ? DarkTheme : LightTheme}>
-      <Page darkModeOn={darkModeOn} setDarkModeOn={setDarkModeOn} />
-    </ThemeProvider>
+  return ( <
+    ThemeProvider theme = {
+      darkModeOn ? DarkTheme : LightTheme
+    } >
+    <
+    Page darkModeOn = {
+      darkModeOn
+    }
+    setDarkModeOn = {
+      setDarkModeOn
+    }
+    /> <
+    /ThemeProvider>
   );
 }
 
